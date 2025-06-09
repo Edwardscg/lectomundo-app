b6e88ff14b5da0eb34642b6f36c40ac0cd529e9a
Edward <Edward@AN515-57>
2025-06-09 09:20:32 -0500

    feat(helper): Agregacion de metodos en DBHelper para ejecutar consultas SQL con parámetros y mapear resultados

--- LectoMundo/src/main/java/com/lectomundo/repository/helper/DBHelper.java
+++ LectoMundo/src/main/java/com/lectomundo/repository/helper/DBHelper.java
@@ -1,9 +1,8 @@
 package com.lectomundo.repository.helper;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHelper {
 
@@ -39,5 +38,92 @@ public class DBHelper {
             System.out.println("Error en DBHelper.manejarEntidad(): " + e.getMessage());
             throw e;
         }
+    }
+
+    // Ejecuta una consulta SQL con parámetros y devuelve un objeto ConsultaResult
+    // que encapsula la conexión, PreparedStatement y ResultSet asociados.
+    public static  ConsultaManager ejecutarConsulta(String sql, Object... parametros) throws SQLException {
+
+        Connection conn = DBConexion.establecerConexion();
+        PreparedStatement ps = conn.prepareStatement(sql);
+
+        for (int i = 0; i < parametros.length; i++) {
+
+            Object obj = parametros[i];
+            int indice = i + 1;
+
+            if (obj == null) {
+
+                ps.setNull(indice, Types.INTEGER);
+            } else if (obj instanceof java.sql.Time) {
+
+                ps.setTime(indice, (java.sql.Time) obj);
+            } else if (obj instanceof java.sql.Date) {
+
+                ps.setDate(indice, (java.sql.Date) obj);
+            } else {
+
+                ps.setObject(indice, obj);
+            }
+        }
+
+        ResultSet rs = ps.executeQuery();
+
+        return new ConsultaManager(conn, ps, rs);
+    }
+
+    // Ejecuta una consulta SQL con parámetros, mapea la primera fila del resultado a un objeto T
+    // mediante un RowMapper, y cierra automáticamente los recursos asociados.
+    public static <T> T obtenerEntidad(String sql, RowMapper<T> mapper, Object... parametros) throws Exception{
+
+        T entidad = null;
+        ConsultaManager cr = null;
+
+        try{
+
+            cr = ejecutarConsulta(sql, parametros);
+            ResultSet rs = cr.getResultSet();
+
+            if(rs.next()){
+
+                entidad = mapper.mapRow(rs);
+            }
+
+        }finally {
+
+            if(cr!=null){
+
+                cr.cerrar();
+            }
+        }
+        return entidad;
+    }
+
+    // Ejecuta una consulta SQL con parámetros, mapea cada fila del ResultSet a un objeto de tipo T
+    // usando un RowMapper, y devuelve una lista con todos los objetos mapeados.
+    public static <T> List<T> obtenerListaEntidad(String sql, RowMapper<T> mapper, Object... parametros) throws Exception{
+
+        List<T> lista = new ArrayList<>();
+
+        ConsultaManager cr = null;
+
+        try{
+
+            cr = ejecutarConsulta(sql, parametros);
+            ResultSet rs = cr.getResultSet();
+
+            while(rs.next()){
+
+                lista.add(mapper.mapRow(rs));
+            }
+
+        }finally {
+            if(cr!=null){
+
+                cr.cerrar();
+            }
+        }
+
+        return lista;
     }
 }
