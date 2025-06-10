5f4f2b76db7cb06ad9ee7534976a9418556c4e46
Edward <Edward@AN515-57>
2025-06-10 12:53:27 -0500

    feat(dao): Agregacion de metodos de busqueda unitaria, especifica y general de Alquiler, metodo de mapeo de objeto Alquiler

--- LectoMundo/src/main/java/com/lectomundo/repository/dao/AlquilerDAO.java
+++ LectoMundo/src/main/java/com/lectomundo/repository/dao/AlquilerDAO.java
@@ -1,10 +1,13 @@
 package com.lectomundo.repository.dao;
 
-import com.lectomundo.model.Alquiler;
+import com.lectomundo.model.*;
 import com.lectomundo.repository.helper.DBHelper;
 
 import java.sql.Date;
+import java.sql.ResultSet;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlquilerDAO {
 
@@ -22,6 +25,42 @@ public class AlquilerDAO {
         String sql = "UPDATE alquiler SET estado = 'finalizado' WHERE id_alquiler = ?";
         DBHelper.manejarEntidad(sql, id_alquiler);
     }
-    
+
+    public Alquiler buscarAlquilerPorId(int id_alquiler) throws Exception {
+
+        String sql = "SELECT * FROM alquiler WHERE id_alquiler = ?";
+
+        return DBHelper.obtenerEntidad(sql, this::mapearAlquiler, id_alquiler);
+    }
+
+    public List<Alquiler> verAlquileres() throws Exception {
+        List<Alquiler> lista_alquileres = new ArrayList<>();
+
+        String sql = "SELECT * FROM alquiler";
+
+        return DBHelper.obtenerListaEntidad(sql, this::mapearAlquiler);
+    }
+
+    public List<Alquiler> verAlquileresPorUsuario(int id_usuario) throws Exception {
+
+        String sql = "SELECT * FROM alquiler WHERE id_usuario = ?";
+
+        return DBHelper.obtenerListaEntidad(sql, this::mapearAlquiler, id_usuario);
+    }
+
+    public List<Alquiler> verAlquileresActivosPorUsuario(int id_usuario) throws Exception {
+
+        String sql = "SELECT * FROM alquiler WHERE id_usuario = ? AND estado = 'activo'";
+
+        return DBHelper.obtenerListaEntidad(sql, this::mapearAlquiler, id_usuario);
+    }
+
+    private Alquiler mapearAlquiler(ResultSet rs) throws Exception{
+
+        Usuario usuario = usuarioDAO.buscarUsuarioPorId(rs.getInt("id_usuario"));
+        Documento documento = documentoDAO.buscarDocumentoPorId(rs.getInt("id_documento"));
+
+        return new Alquiler(rs.getInt("id_alquiler"), (Cliente) usuario, documento, rs.getTimestamp("fecha_inicio").toLocalDateTime(), rs.getTimestamp("fecha_fin").toLocalDateTime(), Estado.valueOf(rs.getString("estado")));
+    }
 
 }
