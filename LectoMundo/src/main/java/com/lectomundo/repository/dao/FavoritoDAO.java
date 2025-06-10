d24eac282fe993268e07ad2430ec932a68eef229
Edward <Edward@AN515-57>
2025-06-10 14:11:10 -0500

    feat(dao): Agreacion de metodo para obtener los objetos Documento favoritos por usuario

--- LectoMundo/src/main/java/com/lectomundo/repository/dao/FavoritoDAO.java
+++ LectoMundo/src/main/java/com/lectomundo/repository/dao/FavoritoDAO.java
@@ -1,11 +1,18 @@
 package com.lectomundo.repository.dao;
 
+import com.lectomundo.model.Documento;
+import com.lectomundo.model.Favorito;
 import com.lectomundo.repository.helper.DBHelper;
+
+import java.sql.ResultSet;
+import java.util.List;
 
 public class FavoritoDAO {
 
+    DocumentoDAO documentoDAO = new DocumentoDAO();
+
     public void agregarFavorito(int id_usuario, int id_documento) throws Exception {
-        
+
         if (esFavorito(id_usuario, id_documento)) {
 
             String sql = "UPDATE favorito SET es_favorito = 1 WHERE id_usuario = ? AND id_documento = ?";
@@ -29,10 +36,19 @@ public class FavoritoDAO {
         }
     }
 
+    public List<Documento> obtenerFavoritosPorUsuario(int id_usuario) throws Exception {
+
+        String sql = "SELECT d.* FROM favorito f JOIN documento d ON f.id_documento = d.id_documento WHERE f.id_usuario = ? AND f.es_favorito = 1";
+
+        return DBHelper.obtenerListaEntidad(sql, documentoDAO::mapearDocumento, id_usuario);
+    }
+
     public boolean esFavorito(int id_usuario, int id_documento) throws Exception {
 
         String sql = "SELECT 1 FROM favorito WHERE id_usuario = ? AND id_documento = ? LIMIT 1";
 
         return DBHelper.obtenerEntidad(sql, rs -> true, id_usuario, id_documento) != null;
     }
+
+    // FUTURA AGREGACION DE METODO PARA OBTENER TODOS LOS REGISTROS E FAVORITO, Y MAPEAR FAVORITO
 }
