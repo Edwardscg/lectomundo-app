120de2302156cb1997088176e81ff27ac22012bf
Edward <Edward@AN515-57>
2025-06-10 14:37:00 -0500

    feat(dao): Agreacion de metodo de mapeo de objeto Valoracion, y metodo de busqueda de objetos Valoracion por documento

--- LectoMundo/src/main/java/com/lectomundo/repository/dao/ValoracionDAO.java
+++ LectoMundo/src/main/java/com/lectomundo/repository/dao/ValoracionDAO.java
@@ -1,9 +1,14 @@
 package com.lectomundo.repository.dao;
 
+import com.lectomundo.model.Cliente;
+import com.lectomundo.model.Documento;
+import com.lectomundo.model.Usuario;
 import com.lectomundo.model.Valoracion;
 import com.lectomundo.repository.helper.DBHelper;
 
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.util.List;
 
 public class ValoracionDAO {
 
@@ -28,5 +33,20 @@ public class ValoracionDAO {
         String sql = "DELETE FROM valoracion WHERE id_usuario = ? AND id_documento = ?;";
 
         DBHelper.manejarEntidad(sql, id_usuario, id_documento);
+    }
+
+    public List<Valoracion> verValoracionesPorDocumento(int id_documento) throws Exception {
+
+        String sql = "SELECT * FROM valoracion WHERE id_documento = ? ORDER BY fecha DESC;";
+
+        return DBHelper.obtenerListaEntidad(sql, this::mapearValoracion, id_documento);
+    }
+
+    private Valoracion mapearValoracion(ResultSet rs) throws Exception{
+
+        Usuario usuario = usuarioDAO.buscarUsuarioPorId(rs.getInt("id_usuario"));
+        Documento documento = documentoDAO.buscarDocumentoPorId(rs.getInt("id_documento"));
+
+        return new Valoracion(rs.getInt("id_valoracion"), (Cliente) usuario, documento, rs.getInt("puntuacion"), rs.getString("comentario"), rs.getDate("fecha_valoracion").toLocalDate());
     }
 }
