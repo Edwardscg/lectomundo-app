e57ede800a18b8d638e5690de6543d6af0df6311
Edward <Edward@AN515-57>
2025-06-10 13:04:56 -0500

    feat(dao): Agregacion de metodo de mapeo de objeto Membresia, y metodos de consulta individuales

--- LectoMundo/src/main/java/com/lectomundo/repository/dao/MembresiaDAO.java
+++ LectoMundo/src/main/java/com/lectomundo/repository/dao/MembresiaDAO.java
@@ -1,10 +1,13 @@
 package com.lectomundo.repository.dao;
 
+import com.lectomundo.model.Cliente;
 import com.lectomundo.model.Estado;
 import com.lectomundo.model.Membresia;
+import com.lectomundo.model.Usuario;
 import com.lectomundo.repository.helper.DBHelper;
 
 import java.sql.Date;
+import java.sql.ResultSet;
 import java.time.LocalDate;
 
 public class MembresiaDAO {
@@ -34,5 +37,28 @@ public class MembresiaDAO {
         String sql = "UPDATE membresia SET estado = ?, fecha_fin = ? WHERE id_membresia = ?";
 
         DBHelper.manejarEntidad(sql, Estado.finazlizado.toString(), Date.valueOf(LocalDate.now()), id_membresia);
+    }
+
+    public Membresia buscarMembresiaPorId(int id_membresia) throws Exception {
+
+        String sql = "SELECT * FROM membresia WHERE id_membresia = ?";
+
+        return DBHelper.obtenerEntidad(sql, this::mapearMembresia, id_membresia);
+    }
+
+    public boolean tieneMembresiaActiva(int id_usuario) throws Exception {
+
+        String sql = "SELECT COUNT(*) FROM membresia WHERE id_usuario = ? AND estado = ?";
+
+        Integer tiene_membresia = DBHelper.obtenerEntidad(sql, rs -> 1, id_usuario, Estado.activo.toString());
+
+        return tiene_membresia !=null;
+    }
+
+    private Membresia mapearMembresia(ResultSet rs) throws Exception{
+
+        Usuario usuario = usuarioDAO.buscarUsuarioPorId(rs.getInt("id_usuario"));
+
+        return new Membresia(rs.getInt("id_membresia"), (Cliente) usuario, rs.getDate("fecha_inicio").toLocalDate(), rs.getDate("fecha_fin").toLocalDate(), rs.getInt("costo"), Estado.valueOf(rs.getString("estado")));
     }
 }
