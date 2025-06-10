6dac0eca09dda6ec50b216e61c24c0da3bc4c03d
Edward <Edward@AN515-57>
2025-06-10 11:09:12 -0500

    feat(dao): Agregacion de metodos de actualizar datos de usuario y actualizacion de monedas de usuario

--- LectoMundo/src/main/java/com/lectomundo/repository/dao/UsuarioDAO.java
+++ LectoMundo/src/main/java/com/lectomundo/repository/dao/UsuarioDAO.java
@@ -20,4 +20,25 @@ public class UsuarioDAO {
             throw new Exception("No se pudo registrar al usuario");
         }
     }
+
+    public void actualizarUsuario(Usuario usuario) throws Exception {
+
+        boolean es_admin = usuario instanceof Administrador;
+
+        String sql = es_admin ? "UPDATE usuario SET nombre = ?, correo = ?, contrasena = ?, tipo = ? WHERE id_usuario = ?" : "UPDATE usuario SET nombre = ?, correo = ?, contrasena = ?, tipo = ?, monedas = ? WHERE id_usuario = ?";
+
+        int fila_afectada = es_admin ? DBHelper.manejarEntidad(sql, usuario.getNombre_usuario(), usuario.getCorreo(), usuario.getContraseña(), usuario.getTipo_usuario(), usuario.getId_usuario()) : DBHelper.manejarEntidad(sql, usuario.getNombre_usuario(), usuario.getCorreo(), usuario.getContraseña(), usuario.getTipo_usuario(), ((Cliente) usuario).getMonedas(), usuario.getId_usuario());
+
+        if (fila_afectada == 0) {
+
+            throw new Exception("No se pudo actualizar el usuario.");
+        }
+    }
+
+    public void actualizarMonedas(int id_usuario, int nuevas_monedas) throws Exception {
+
+        String sql = "UPDATE usuario SET monedas = ? WHERE id_usuario = ?";
+
+        DBHelper.manejarEntidad(sql, nuevas_monedas, id_usuario);
+    }
 }
