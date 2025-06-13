31220dd99e8ca7a395997a38bc2f6e4fac7d69bd
Edward <Edward@AN515-57>
2025-06-13 08:21:17 -0500

    feat(login): Adicion de metodo de incio de sesion en clase LoginControlador

--- LectoMundo/src/main/java/com/lectomundo/controller/general/LoginControlador.java
+++ LectoMundo/src/main/java/com/lectomundo/controller/general/LoginControlador.java
@@ -1,5 +1,41 @@
 package com.lectomundo.controller.general;
 
+import com.lectomundo.controller.UIHelper;
+import com.lectomundo.logic.UsuarioService;
+import com.lectomundo.model.Usuario;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
 public class LoginControlador {
 
+    @FXML private TextField txtCorreo;
+    @FXML private PasswordField txtContraseña;
+    @FXML private Hyperlink hyperlinkrecuperarContraseña;
+
+    @FXML
+    private void IniciarSesion() throws Exception {
+
+        String correo = txtCorreo.getText();
+        String contraseña = txtContraseña.getText();
+
+        if(correo.isEmpty() || contraseña.isEmpty()){
+
+            UIHelper.mostrarAlerta("Entradas vacías", "Llene todos los campos.");
+            return;
+        }
+
+        UsuarioService usuarioService = new UsuarioService();
+        Usuario usuario = usuarioService.loguearUsuario(correo, contraseña);
+
+        if(usuario!=null){
+
+            Stage ventana_actual = (Stage) txtCorreo.getScene().getWindow();
+            UIHelper.abrirVentana(ventana_actual, "/view/admin/PanelAdministrador.fxml", "Administrador");
+
+        }else {
+
+            UIHelper.mostrarAlerta("Error", "Correo o contraseña incorrectos.");
+        }
+    }
 }
