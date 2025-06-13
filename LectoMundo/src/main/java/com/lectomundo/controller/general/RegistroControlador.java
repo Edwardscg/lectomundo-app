a291ab57bf50e3b68552803c1acaeae65f585f7d
Edward <Edward@AN515-57>
2025-06-13 08:43:06 -0500

    Creacion de fxml generico de codigoVerificador y su controlador

--- LectoMundo/src/main/java/com/lectomundo/controller/general/RegistroControlador.java
+++ LectoMundo/src/main/java/com/lectomundo/controller/general/RegistroControlador.java
@@ -1,4 +1,16 @@
 package com.lectomundo.controller.general;
 
+import com.lectomundo.controller.UIHelper;
+import com.lectomundo.logic.CorreoService;
+import com.lectomundo.logic.UsuarioService;
+import com.lectomundo.model.Usuario;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
 public class RegistroControlador {
+
 }
