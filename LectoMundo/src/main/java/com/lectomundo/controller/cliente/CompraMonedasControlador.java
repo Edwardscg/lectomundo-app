0c443c04634b81fa77a2969fa4901c6c15337246
Edward <Edward@AN515-57>
2025-06-17 00:51:38 -0500

    feat(cliente): agregar metodos de compra de monedas segun cantidad en clase CompraMonedasControlador

--- LectoMundo/src/main/java/com/lectomundo/controller/cliente/CompraMonedasControlador.java
+++ LectoMundo/src/main/java/com/lectomundo/controller/cliente/CompraMonedasControlador.java
@@ -1,4 +1,62 @@
 package com.lectomundo.controller.cliente;
 
+import com.lectomundo.controller.UIHelper;
+import com.lectomundo.logic.MovimientoMonedaService;
+import com.lectomundo.model.Cliente;
+import javafx.fxml.FXML;
+
 public class CompraMonedasControlador {
+
+    private MovimientoMonedaService movimientoMonedaService = new MovimientoMonedaService();
+    private Cliente cliente = ClienteControlador.cliente;
+
+    @FXML
+    private void comprar60Monedas(){
+
+        comprarMonedas(60);
+    }
+
+    @FXML
+    private void comprar300Monedas(){
+
+        comprarMonedas(300);
+    }
+
+    @FXML
+    private void comprar980Monedas(){
+
+        comprarMonedas(980);
+    }
+
+    @FXML
+    private void comprar1980Monedas(){
+
+        comprarMonedas(1980);
+    }
+
+    @FXML
+    private void comprar3280Monedas(){
+
+        comprarMonedas(3280);
+    }
+
+    @FXML
+    private void comprar6480Monedas(){
+
+        comprarMonedas(6480);
+    }
+
+    private void comprarMonedas(int monto){
+
+        try{
+
+            movimientoMonedaService.comprarMonedas(cliente, monto);
+            UIHelper.mostrarAlerta("Compra exitosa", "Se han comprado " + monto + " monedas.");
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+            UIHelper.mostrarAlerta("Error", "No se pudo completar la compra.");
+        }
+    }
 }
