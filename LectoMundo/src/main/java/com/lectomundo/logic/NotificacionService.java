4340c6fe082c80cc95df8df8883e5cbc2442462b
Edward <Edward@AN515-57>
2025-06-26 23:56:14 -0500

    feat(cliente): crear metodos de NotificacionService(obtener notificaciones, crear notificacion por tipo)

--- LectoMundo/src/main/java/com/lectomundo/logic/NotificacionService.java
+++ LectoMundo/src/main/java/com/lectomundo/logic/NotificacionService.java
@@ -1,8 +1,11 @@
 package com.lectomundo.logic;
 
 import com.lectomundo.model.Cliente;
+import com.lectomundo.model.Documento;
 import com.lectomundo.model.Notificacion;
 import com.lectomundo.repository.dao.NotificacionDAO;
+
+import java.util.List;
 
 public class NotificacionService {
 
@@ -18,5 +21,37 @@ public class NotificacionService {
         notificacionDAO.registrarNotificacion(notificacion);
     }
 
+    public List<Notificacion> obtenerNotificacionesPorUsuario(int id_usuario){
 
+        return notificacionDAO.obtenerNotificacionesPorUsuario(id_usuario);
+    }
+
+    public void marcarComoLeida(int id_notificacion){
+
+        notificacionDAO.marcarNotificacionComoLeida(id_notificacion);
+    }
+
+    public void notificacionCompraDocumento(Cliente cliente, Documento documento){
+
+        String mensaje = "Has comprado el documento: " + documento.getTitulo();
+        registrarNotificacion(cliente, "Compra", mensaje);
+    }
+
+    public void notificacionAlquilerDocumento(Cliente cliente, Documento documento){
+
+        String mensaje = "Has alquilado el documento: " + documento.getTitulo();
+        registrarNotificacion(cliente, "Alquiler", mensaje);
+    }
+
+    public void notificacionUltimoDiaAlquiler(Cliente cliente, Documento documento){
+
+        String mensaje = "Hos es el ultimo día de alquiler del documento: " + documento.getTitulo();
+        registrarNotificacion(cliente, "Aviso-Alquiler", mensaje);
+    }
+
+    public void notificacionUltimoDiaMembresia(Cliente cliente){
+
+        String mensaje = "Hoy es el ultimo dia de tu membresía, renueva para seguir teniendo acceso completo a los documentos.";
+        registrarNotificacion(cliente, "Aviso-Membresía", mensaje);
+    }
 }
