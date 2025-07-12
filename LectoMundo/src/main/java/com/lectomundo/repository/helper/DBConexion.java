134f6deb02ee905d166e92cfa4b0ffe417457490
Edward <Edward@AN515-57>
2025-07-12 10:36:52 -0500

    fix: proteger claves de configuración

--- LectoMundo/src/main/java/com/lectomundo/repository/helper/DBConexion.java
+++ LectoMundo/src/main/java/com/lectomundo/repository/helper/DBConexion.java
@@ -1,18 +1,49 @@
 package com.lectomundo.repository.helper;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
+/**
+ * Clase auxiliar encargada de establecer la conexión con la base de datos TiDB Cloud.
+ * Centraliza la configuración de conexión para evitar la repetición de credenciales y URLs.
+ */
 public class DBConexion {
 
-    private static final String URL = "jdbc:mysql://gateway01.us-east-1.prod.aws.tidbcloud.com:4000/biblioteca_virtual";
-    private static final String user = "2nrjKr1bzyGsGm5.root";
-    private static final String password = "deKZTb4j3LAOhlMK";
+    private static String jdbc_url;
+    private static String bd_user;
+    private static String bd_password;
 
-    // Establece y devuelve una conexión a la base de datos TiDB usando los parámetros definidos.
+    static {
+
+        try{
+
+            Properties props = new Properties();
+
+            // Ruta del archivo de configuración
+            FileInputStream fis = new FileInputStream("db.properties");
+
+            props.load(fis);
+            jdbc_url = props.getProperty("JDBC_URL");
+            bd_user= props.getProperty("BD_USER");
+            bd_password = props.getProperty("BD_PASSWORD");
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Establece una conexión con la base de datos TiDB Cloud usando JDBC.
+     *
+     * @return objeto Connection listo para usar en operaciones SQL
+     * @throws SQLException si no se puede establecer la conexión
+     */
     public static Connection establecerConexion() throws SQLException {
 
-        return DriverManager.getConnection(URL, user, password);
+        return DriverManager.getConnection(jdbc_url, bd_user, bd_password);
     }
 }
